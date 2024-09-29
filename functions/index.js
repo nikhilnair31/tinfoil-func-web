7a1af58feb7f5e3c6df04ccc5012a97662f5d8e1
Nikhil Nair <niknair31898@gmail.com>
2024-09-29 13:32:54 -0400

    cleaned

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,3 @@
-/**
-    * Import function triggers from their respective submodules:
-    *
-    * const {onCall} = require("firebase-functions/v2/https");
-    * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
-    *
-    * See a full list of supported triggers at https://firebase.google.com/docs/functions
-*/
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
 const {onRequest} = require("firebase-functions/v2/https");
 const functions = require('firebase-functions')
 const logger = require("firebase-functions/logger");
@@ -20,31 +8,19 @@ const openai = new OpenAI({
     apiKey:  config.openai_api_key
 });
 
-const modelName = `gpt-4o-mini`
-const systemPrompt = `
+// Scoring parameters
+const modelName_scoring = `gpt-4o-mini`
+const systemPrompt_scoring = `
 You are to work on a fun and absurd conspiracy creator game. 
 The player is to be presented with a list of things like notes, news snippets, images and location markers. 
 They can then connect them in any order with some relationship between the connections. 
 This final conspiracy is to be judged and scored to rank the player on a global leaderboard. 
 You now have to stitch together the multiple connected clues into a single cohesive conspiracy.
 `;
-const temp = 0.3
-const max_tok = 4096
+const temp_scoring = 0.3
+const max_tok_scoring = 4096
 
-// exports.helloWorld = onRequest((request, response) => {
-//     logger.info("Hello logs!", {structuredData: true});
-//     response.send("Hello from Firebase!");
-// });
-
-exports.callOpenaiApi = onRequest(async (request, response) => {
-    const prompt = request.query.text;
-    if (prompt.length == 0) {
-        response.status(400).send("Missing prompt");
-    }
-    else {
-        logger.info(`Prompt: ${prompt}`);
-    }
-    
+async function CallOpenaiApi(modelName, systemPrompt, userPrompt, temp, max_tok) {
     const completion = await openai.chat.completions.create({
         model: modelName,
         messages: [
@@ -54,13 +30,35 @@ exports.callOpenaiApi = onRequest(async (request, response) => {
             },
             {
                 "role": "user", 
-                "content": prompt
+                "content": userPrompt
             }
         ],
         temperature: temp,
         max_completion_tokens: max_tok
     });
-    logger.info(`Choice: ${completion.choices[0]}`);
+    logger.info(`CallOpenaiApi Choice: ${completion.choices[0]}`);
 
-    response.send(completion.choices[0]);
+    return completion.choices[0];
+}
+
+exports.callScoring = functions.https.onRequest(async (request, response) => {
+    const prompt = request.query.text;
+    if (prompt.length == 0) {
+        response.status(400).send("Missing prompt");
+    }
+    else {
+        logger.info(`Prompt: ${prompt}`);
+    }
+    
+    const completion_scoring = await CallOpenaiApi(
+        modelName_scoring, 
+        systemPrompt_scoring, 
+        prompt_scoring, 
+        temp_scoring, 
+        max_tok_scoring
+    );
+    const result_scoring = completion_scoring.choices[0].content;
+    logger.info(`Scoring Result: ${result_scoring}`);
+
+    response.send(result_scoring);
 });
